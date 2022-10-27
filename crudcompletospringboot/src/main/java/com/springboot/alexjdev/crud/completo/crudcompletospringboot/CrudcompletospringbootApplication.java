7802425c9c284d782e24e6b944c5745c45d783a5
Andre Matos <andreluisv8@gmail.com>
2022-10-26 23:32:48 -0300

    Spring Boot project created

---
+++ crudcompletospringboot/src/main/java/com/springboot/alexjdev/crud/completo/crudcompletospringboot/CrudcompletospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.alexjdev.crud.completo.crudcompletospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcompletospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcompletospringbootApplication.class, args);
+	}
+
+}
---
+++ crudcompletospringboot/src/test/java/com/springboot/alexjdev/crud/completo/crudcompletospringboot/CrudcompletospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.alexjdev.crud.completo.crudcompletospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcompletospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
